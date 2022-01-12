63542563bf81a08d757383c567bc4ee53db9fce7
sully <sullivankuhfahl@gmail.com>
2022-01-11 18:24:39 -0600

    :D

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ import DiscordJS, { Intents } from 'discord.js'
 import dotenv from 'dotenv'
 dotenv.config()
 
+const fs = require('fs');
+const { Client, Collection, Intents } = require('discord.js');
+
 const client = new DiscordJS.Client({
     intents: [ 
         Intents.FLAGS.GUILDS,
@@ -9,8 +12,22 @@ const client = new DiscordJS.Client({
     ]
 })
 
-client.on("Ready", () => {
+client.once("Ready", () => {
     console.log("Bot's ready :D")
 })
+
+client.on("messageCreate", (message) => {
+    // example onmessage event
+})
+
+client.commands = new Collection();
+const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+
+for (const file of commandFiles) {
+	const command = require(`./commands/${file}`);
+	// Set a new item in the Collection
+	// With the key as the command name and the value as the exported module
+	client.commands.set(command.data.name, command);
+}
 
 client.login(process.env.TOKEN)
